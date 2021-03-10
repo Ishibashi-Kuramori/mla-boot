b49e83edaa92f4f461071fb0b178a45bedf867cd
Ishibashi-Kuramori <kisibasi0331@gmail.com>
2021-03-10 12:24:34 +0900

    未実装機能の仮ページを設置

---
+++ src/main/java/jp/ken/mla/controller/StockController.java
@@ -0,0 +1,30 @@
+package jp.ken.mla.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+import jp.ken.mla.model.LoginModel;
+
+@Controller
+@RequestMapping("stock")
+@SessionAttributes("loginModel")
+public class StockController {
+
+	// 在庫管理画面表示
+	@RequestMapping(method=RequestMethod.GET)
+	public String toAddMember(Model model, LoginModel lModel) {
+		if(lModel.getMember_id() == 0) { // 未ログイン時はログイン画面にリダイレクト
+			return "redirect:/login";
+		}
+		if(lModel.getAdmin() != 1) { // 管理ユーザ以外からのアクセスはエラー
+			model.addAttribute("errorMessage", "管理者ユーザ以外はアクセス出来ません。");
+			IndexController.setActiveTab(model, "error");
+			return "index";
+		}
+		IndexController.setActiveTab(model, "stock");
+		return "index";
+	}
+}
